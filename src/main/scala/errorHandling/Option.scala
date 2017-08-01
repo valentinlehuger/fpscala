e9c0db160df3ebebd822fe88f6dca11a63c59d4b
Valentin Lehuger <valentin@lehuger.fr>
2017-08-01 10:02:04 +0200

    added test for option

--- src/main/scala/errorHandling/Option.scala
+++ src/main/scala/errorHandling/Option.scala
@@ -6,6 +6,26 @@ trait Option[+A] {
         case None => None
         case Some(a) => Some(f(a))
     }
+
+    def flatMap[B](f:A => Option[B]): Option[B] = this match {
+        case None => None
+        case Some(a) => f(a)
+    }
+
+    def getOrElse[B >: A](default: => B): B = this match {
+        case None => default
+        case Some(a) => a
+    }
+
+    def orElse[B >: A](default: => Option[B]): Option[B] = this match {
+        case None => default
+        case _ => this
+    }
+
+    def filter(f: A => Boolean): Option[A] = this match {
+        case Some(a) if f(a) => this
+        case _ => None
+    }
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -15,5 +35,4 @@ object Option {
     def mean(xs: Seq[Double]): Option[Double] =
         if (xs.isEmpty) None
         else Some(xs.sum / xs.length)
-
 }
--- src/test/scala/errorHandling/OptionTest.scala
+++ src/test/scala/errorHandling/OptionTest.scala
@@ -16,4 +16,44 @@ class OptionTest extends FlatSpec with Matchers {
     "Some(3).map(_ => _ + 1)" should "be Some(4)" in {
         Some(3).map(a => a + 1) should be (Some(4))
     }
+
+    "None.map(_ => _ + 1)" should "be None" in {
+        None.map(a => a) should be (None)
+    }
+
+    "Some(3).flatMap(_ => _ + 1)" should "be Some(4)" in {
+        Some(3).flatMap(a => Some(a + 1)) should be (Some(4))
+    }
+
+    "Some(3).flatMap(_ => None)" should "be None" in {
+        Some(3).flatMap(a => None) should be (None)
+    }
+    
+    "Some(3).getOrElse(2)" should "be 3" in {
+        Some(3).getOrElse(2) should be (3)
+    }
+
+    "None.getOrElse(2)" should "be 2" in {
+        None.getOrElse(2) should be (2)
+    }
+
+    "Some(3).orElse(Some(2))" should "be Some(3)" in {
+        Some(3).orElse(Some(2)) should be (Some(3))
+    }
+
+    "None.orElse(Some(2))" should "be Some(2)" in {
+        None.orElse(Some(2)) should be (Some(2))
+    }
+
+    "Some(3).filter(x => x < 2)" should "None" in {
+        Some(3).filter(x => x < 2) should be (None)
+    }
+    
+    "Some(1).filter(x => x < 2)" should "Some(1)" in {
+        Some(1).filter(x => x < 2) should be (Some(1))
+    }
+
+    "None.filter(x => x < 2)" should "None" in {
+        None.filter(x => true) should be (None)
+    }
 }

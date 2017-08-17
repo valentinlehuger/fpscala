ea25ef9330ad2876f87e3e630537e197432e652a
Valentin Lehuger <valentin@lehuger.fr>
2017-08-17 22:54:37 +0200

    added map for random

--- src/main/scala/purelyFunctionalState/random.scala
+++ src/main/scala/purelyFunctionalState/random.scala
@@ -52,4 +52,24 @@ object RNG {
             val (tail, next2) = ints(count - 1)(next)
             (n :: tail, next2)
         }
+
+    type Rand[+A] = RNG => (A, RNG)
+
+    val int: Rand[Int] = _.nextInt
+
+
+    def unit[A](a: A): Rand[A] =
+        rng => (a, rng)
+
+    def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+        rng => {
+            val (a, rng2) = s(rng)
+            (f(a), rng2)
+        }
+
+    def nonNegativeEven: Rand[Int] =
+        map(nonNegativeInt)(i => i - i % 2)
+
+    def double_2: Rand[Double] =
+        map(nonNegativeInt)(i => i / Int.MaxValue)
 }
